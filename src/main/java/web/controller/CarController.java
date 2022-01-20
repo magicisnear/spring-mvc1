523753ff1b107ef31fac5e5bc04eb1119f91e288
funre <magicisnear@bk.ru>
2022-01-20 22:35:24 +0300

    new

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,20 +1,25 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarDao;
-import web.service.CarDaoImpl;
 
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    CarDao carDao = new CarDaoImpl();
+    private final CarDao carDao;
+
+    @Autowired
+    public CarController(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
 
     @GetMapping(value = "/cars")
     public String CarList(@RequestParam(value = "count", required = true, defaultValue = "5") int count, Model model) {
